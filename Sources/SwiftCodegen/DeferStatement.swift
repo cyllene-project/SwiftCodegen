a4eaf7da9ce0cfd66e2955f25eb8ca245f6d4a58
Chris Daley <chebizarro@gmail.com>
2018-01-11 08:46:57 -0800

    added DeferStatement

---
+++ Sources/SwiftCodegen/DeferStatement.swift
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+// DeferStatement.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2018 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
+public class DeferStatement: CodeNode {
+	
+	public var body: Block
+	
+	public init(body: Block, sourceReference: SourceReference? = nil) {
+		self.body - body
+		super.init()
+		self.sourceReference = sourceReference 
+	}
+	
+	
+}
