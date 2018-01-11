cb00ced28213566e0f8660cd83b5f9f8cf3be75c
Chris Daley <chebizarro@gmail.com>
2018-01-11 10:22:25 -0800

    added GenericType class

---
+++ Sources/SwiftCodegen/GenericType.swift
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+// GenericType.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2018 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
+public class GenericType: DataType {
+	
+	
+	public init(typeParameter: DataType) {
+		super.init()
+		self.typeParameter = typeParameter
+	}
+	
+	
+}
