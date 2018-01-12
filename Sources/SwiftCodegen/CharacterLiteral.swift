b007292d85ba78ba2f7f37d59f4dbb537347765f
Chris Daley <chebizarro@gmail.com>
2018-01-11 23:16:44 -0800

    added CharacterLiteral class

---
+++ Sources/SwiftCodegen/CharacterLiteral.swift
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+// CharacterLiteral.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2018 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
+public class CharacterLiteral: Literal {
+
+	public var value: String
+	
+	public init(value: String, sourceReference: SourceReference? = nil) {
+		self.value = value
+		super.init(sourceReference: sourceReference)
+	}
+	
+	public override func accept<T: CodeWriter>(visitor: T) {
+		visitor.writeString(value)
+	}
+
+	
+}
