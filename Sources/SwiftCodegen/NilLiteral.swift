b2b6e6868330c13d139914de0eb6ba27c2a8c451
Chris Daley <chebizarro@gmail.com>
2018-01-11 23:18:49 -0800

    added NilLiteral class

---
+++ Sources/SwiftCodegen/NilLiteral.swift
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+// NilLiteral.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2018 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
+public class NilLiteral: Literal {
+		
+	public override func accept<T: CodeWriter>(visitor: T) {
+		visitor.writeString("nil")
+	}
+
+}
