0d6dad67627de5d96970e476a9fe7797545315c5
Chris Daley <chebizarro@gmail.com>
2018-01-11 19:43:57 -0800

    added TryStatement class

---
+++ Sources/SwiftCodegen/TryStatement.swift
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+// TryStatement.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2018 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
+public class TryStatement: CodeNode, Statement {
+	
+	public var expression: Expression
+	
+	public init(expression: Expression, sourceReference: SourceReference) {
+		self.expression = expression
+		super.init(sourceReference: sourceReference)
+	}
+	
+	
+}
