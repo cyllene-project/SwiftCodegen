9649618c41d514ea5191ba32bfb429ab6199fedd
Chris Daley <chebizarro@gmail.com>
2018-01-15 11:58:49 -0800

    added various Variable and Constant unit tests

--- Tests/SwiftCodegenTests/SwiftCodegenTests.swift
+++ Tests/SwiftCodegenTests/SwiftCodegenTests.swift
@@ -9,12 +9,11 @@ class SwiftCodegenTests: XCTestCase {
 
 		let writer = TestWriter()
 		
-		let const = Constant(let: "maximumNumberOfLoginAttempts", equals: 10)
+		let const = Constant(name: "maximumNumberOfLoginAttempts", value: 10)
 		
 		const.accept(visitor: writer)
 
 		XCTAssertEqual(writer.content, result, "Output did not mach expected result")
-		
 	}
 	
 	func testVar() {
@@ -23,14 +22,83 @@ class SwiftCodegenTests: XCTestCase {
 
 		let writer = TestWriter()
 		
-		let variable = Variable(var: "currentLoginAttempt", equals: 0)
+		let variable = Variable(name: "currentLoginAttempt", value: 0)
 
 		vairable.accept(visitor: writer)
 
-		XCTAssertEqual(writer.content, result, "Output did not mach expected result")
-		
-		
+		XCTAssertEqual(writer.content, result, "Output did not mach expected result")		
 	}
+	
+	func testMultiVar() {
+		
+		let result = "var x = 0.0, y = 0.0, z = 0.0\n"
+
+		let writer = TestWriter()
+		
+		let variable = Variable(name: "x", value: 0.0)
+
+		vairable.accept(visitor: writer)
+
+		XCTAssertEqual(writer.content, result, "Output did not mach expected result")		
+	}
+	
+	func testVarWithType() {
+		
+		let result = "var welcomeMessage: String\n"
+
+		let writer = TestWriter()
+		
+		let variable = Variable(name: "welcomeMessage")
+
+		vairable.accept(visitor: writer)
+
+		XCTAssertEqual(writer.content, result, "Output did not mach expected result")		
+	}
+	
+	func testVarAssignment() {
+		
+		let result = "welcomeMessage = \"Hello\"\n"
+
+		let writer = TestWriter()
+		
+		let variable = Variable(name: "welcomeMessage")
+
+		vairable.accept(visitor: writer)
+
+		XCTAssertEqual(writer.content, result, "Output did not mach expected result")		
+	}
+	
+	func testMultiVarWithType() {
+		
+		let result = "var red, green, blue: Double\n"
+
+		let writer = TestWriter()
+		
+		let variable = Variable(name: "red")
+
+		vairable.accept(visitor: writer)
+
+		XCTAssertEqual(writer.content, result, "Output did not mach expected result")		
+	}
+	
+	func testUnicodeNames() {
+		
+		let result = """
+		let π = 3.14159
+		let 你好 = \"你好世界\"
+		let �� = \"dogcow\"
+		"""
+
+		let writer = TestWriter()
+		
+		let variable = Variable(name: "red")
+
+		vairable.accept(visitor: writer)
+
+		XCTAssertEqual(writer.content, result, "Output did not mach expected result")		
+	}
+	
+	
 	
 	func testExample() {
 		
