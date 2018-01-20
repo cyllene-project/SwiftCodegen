1cb1d713dc891ba3a277a7e8ec62bc991a696de4
Chris Daley <chebizarro@gmail.com>
2018-01-19 18:49:57 -0800

    added Symbol and SymbolAccessibility to Codex

---
+++ Sources/Codex/Symbol.swift
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+// Symbol.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2018 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
+public protocol Symbol : CodeNode {
+	
+	var name: String? { get set }
+	
+	var deprecated: Bool { get set }
+
+	var access: SymbolAccessibility { get set }
+	
+	var comment: Comment? { get set }
+	
+}
