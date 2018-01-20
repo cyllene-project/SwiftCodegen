df3660b975842e6101f2659fa9805a07696258c0
Chris Daley <chebizarro@gmail.com>
2018-01-20 09:28:06 -0800

    more project reorganization

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftCodegen",
+    name: "Codex",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SwiftCodegen",
-            targets: ["SwiftCodegen"]),
+            name: "Codex",
+            targets: ["Codex", "SwiftLang"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftCodegen",
+            name: "Codex",
             dependencies: []),
+        .target(
+            name: "SwiftLang",
+            dependencies: ["Codex"]),
         .testTarget(
-            name: "SwiftCodegenTests",
-            dependencies: ["SwiftCodegen"]),
+            name: "CodexTests",
+            dependencies: ["Codex"]),
     ]
 )
